00bd49ae08b91b00c34f1bd7591f80a513594c27
ElizPN <pankevych.liza@gmail.com>
2022-04-27 10:59:56 +0200

    counter in class component

---
+++ src/tasks/Counter.js
@@ -0,0 +1,52 @@
+import React from "react";
+// import React, { Component } from "react";
+// import React, { useState } from "react";
+
+export class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    //bining handels with "this"
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  // set new state through handlers
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+
+  decrement() {
+    this.setState((state) => ({
+      count: state.count - 1,
+    }));
+  }
+
+  reset() {
+    this.setState({
+      count: 0,
+    });
+  }
+
+  render() {
+    return (
+      //call handlers in onClick
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+}
