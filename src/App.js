ed4868d97575e6e1f0159d0f5bc2068c5feb1acc
ElizPN <pankevych.liza@gmail.com>
2022-04-28 10:41:18 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import { Counter } from "./tasks/Counter";
+import { MyForm } from "./tasks/MyForm";
+import { MyApp2 } from "./tasks/MyApp2";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React YYYyhhaaa
-        </a>
-      </header> */}
       <Counter />
+      <MyForm />
+      <MyApp2 />
     </div>
   );
 }
