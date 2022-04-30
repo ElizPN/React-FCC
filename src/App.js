fecf7e0dc70a8525158549384ee1b9fa72e54ed9
ElizPN <pankevych.liza@gmail.com>
2022-04-30 12:43:33 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import "./App.css";
 import { Counter } from "./tasks/Counter";
 import { MyForm } from "./tasks/MyForm";
 import { MyApp2 } from "./tasks/MyApp2";
+import { MyApp } from "./tasks/MyApp";
+import { Controller, OnlyEvens } from "./tasks/life_cycle_methods/OnlyEvens";
+import { MagicEightBall } from "./tasks/MagicEightBall.js";
+import { CheckUserAge } from "./tasks/CkeckUserAge";
+import { GateKeeper } from "./tasks/GateKeeper";
+import { MyToDoList } from "./tasks/MyToDoList";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <hr></hr>
       <MyForm />
+      <hr></hr>
+      <MyApp />
+      <hr></hr>
       <MyApp2 />
+      <hr></hr>
+      <Controller />
+      error
+      <hr></hr>
+      <MagicEightBall />
+      <hr></hr>
+      <CheckUserAge />
+      <hr></hr>
+      <GateKeeper />
+      <hr></hr>
+      <MyToDoList />
     </div>
   );
 }
