72ff74b4295c18482e1041990343a0c35841f255
ElizPN <pankevych.liza@gmail.com>
2022-04-30 12:22:05 +0200

    renderToString

---
+++ src/tasks/renderToString.js
@@ -0,0 +1,12 @@
+//Render React on the Server with renderToString
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+ReactDOMServer.renderToString(<App />);
